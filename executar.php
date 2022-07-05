fb847e557b98ae0302f1497b8bbb74a0f15091e5
gili-julio <108534731+gili-julio@users.noreply.github.com>
2022-07-04 23:00:57 -0300

    Update executar.php

--- executar.php
+++ executar.php
@@ -2,18 +2,18 @@
 
 include_once('/secundario/config.php');
 
-$sql = "CREATE TABLE `produtos` (
-  `codigo_barras` TEXT NOT NULL,
-  `cod_produto` TEXT NULL,
-  `descricao` TEXT NULL,
-  `embalagem` TEXT NULL,
-  `cod_fornecedor` TEXT NULL,
-  `estoque` TEXT NULL,
-  `departamento` TEXT NULL,
-  `rua` TEXT NULL,
-  `apartamento` TEXT NULL,
-  `nome_foto` TEXT NULL,
-  PRIMARY KEY (`codigo_barras`))";
+$sql = "CREATE TABLE produtos (
+  codigo_barras TEXT NOT NULL,
+  cod_produto TEXT NULL,
+  descricao TEXT NULL,
+  embalagem TEXT NULL,
+  cod_fornecedor TEXT NULL,
+  estoque TEXT NULL,
+  departamento TEXT NULL,
+  rua TEXT NULL,
+  apartamento TEXT NULL,
+  nome_foto TEXT NULL,
+  PRIMARY KEY (codigo_barras))";
 $result = $conexao->query($sql);
 
 
