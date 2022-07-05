09f4fab47a28128f1e3923797ac2e5ccf66676ff
gili-julio <108534731+gili-julio@users.noreply.github.com>
2022-07-04 23:27:29 -0300

    Update executar.php

--- executar.php
+++ executar.php
@@ -1,20 +1,6 @@
 <?php 
 
-include_once('secundario/config.php');
-
-$sql = "CREATE TABLE heroku_0975b1a2f8429b3.produtos (
-  codigo_barras TEXT NOT NULL,
-  cod_produto TEXT NULL,
-  descricao TEXT NULL,
-  embalagem TEXT NULL,
-  cod_fornecedor TEXT NULL,
-  estoque TEXT NULL,
-  departamento TEXT NULL,
-  rua TEXT NULL,
-  apartamento TEXT NULL,
-  nome_foto TEXT NULL,
-  PRIMARY KEY (codigo_barras))";
-$result = $conexao->query($sql);
+header('Location: index.php')
 
 
 ?>
